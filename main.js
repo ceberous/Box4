794349237350c410948c47fb773963f7de48dac9
morpheous <cerbus.collin@gmail.com>
2018-09-16 12:20:34 -0700

    91

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//console.log = function( msg ){ console.trace();  };
+console.log = function( msg ){ console.trace();  };
 
 process.on( "unhandledRejection" , function( reason , p ) {
 	console.error( reason, "Unhandled Rejection at Promise" , p );
