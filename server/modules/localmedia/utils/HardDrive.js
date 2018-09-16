223ffc2d91a5400a0c7f815ef6be278eec8a9cc6
morpheous <cerbus.collin@gmail.com>
2018-09-15 17:59:03 -0700

    12

--- server/modules/localmedia/utils/HardDrive.js
+++ server/modules/localmedia/utils/HardDrive.js
@@ -3,6 +3,7 @@ const path = require( "path" );
 const Redis = require( path.join( MainFP , "main.js" ) ).redis;
 const RC = Redis.c.LOCAL_MEDIA;
 
+const dirTree = require( "directory-tree" );
 require( "shelljs/global" );
 const FS = require( "fs" );
 const exfs = require("extfs");
@@ -74,25 +75,110 @@ function FIND_USB_STORAGE_PATH_FROM_UUID( wUUID ) {
 		catch( error ) { console.log( error ); reject( error ); }
 	});
 }
-
-
-var sd = null;
-var fr = {};
-function safeReadDirSync(r){var n={};try{n=FS.readdirSync(r)}catch(c){if("EACCES"==c.code)return null;throw c}return n}
-// FROM --> https://github.com/mihneadb/node-directory-tree/blob/master/lib/directory-tree.js
-function directoryTree_Stage1(e,r,n){var t,i=PATH.basename(e),a={name:i};try{t=FS.statSync(e)}catch(u){return null}if(t.isFile()){PATH.extname(e).toLowerCase()}else{if(!t.isDirectory())return null;var l=safeReadDirSync(e);if(null===l)return null;a.children=l.map(function(t){return directoryTree_Stage1(PATH.join(e,t),r,n)}).filter(function(e){return!!e})}return a}
-function directoryTree_Stage2(){for(var e=0;e<sd.children.length;++e){fr[sd.children[e].name]={};for(var d=0;d<sd.children[e].children.length;++d)if(fr[sd.children[e].name][sd.children[e].children[d].name]=[],sd.children[e].children[d].children)for(var a=0;a<sd.children[e].children[d].children.length;++a){var r=[];if(sd.children[e].children[d].children[a].children)for(var n=0;n<sd.children[e].children[d].children[a].children.length;++n)r.push(sd.children[e].children[d].children[a].children[n].name);fr[sd.children[e].name][sd.children[e].children[d].name].push(r)}}}
-
-
-async function BUILD_HD_REF( wMountPoint ) {
-	sd = directoryTree_Stage1( wMountPoint );
-	console.log( sd );
-	directoryTree_Stage2();
-	return fr;
-}
-
 module.exports.findAndMountUSB_From_UUID = FIND_USB_STORAGE_PATH_FROM_UUID;
-module.exports.buildHardDriveReference = BUILD_HD_REF;
+
+
+// var sd = null;
+// var fr = {};
+// function safeReadDirSync(r){var n={};try{n=FS.readdirSync(r)}catch(c){if("EACCES"==c.code)return null;throw c}return n}
+// // FROM --> https://github.com/mihneadb/node-directory-tree/blob/master/lib/directory-tree.js
+// function directoryTree_Stage1(e,r,n){var t,i=PATH.basename(e),a={name:i};try{t=FS.statSync(e)}catch(u){return null}if(t.isFile()){PATH.extname(e).toLowerCase()}else{if(!t.isDirectory())return null;var l=safeReadDirSync(e);if(null===l)return null;a.children=l.map(function(t){return directoryTree_Stage1(PATH.join(e,t),r,n)}).filter(function(e){return!!e})}return a}
+// function directoryTree_Stage2(){for(var e=0;e<sd.children.length;++e){fr[sd.children[e].name]={};for(var d=0;d<sd.children[e].children.length;++d)if(fr[sd.children[e].name][sd.children[e].children[d].name]=[],sd.children[e].children[d].children)for(var a=0;a<sd.children[e].children[d].children.length;++a){var r=[];if(sd.children[e].children[d].children[a].children)for(var n=0;n<sd.children[e].children[d].children[a].children.length;++n)r.push(sd.children[e].children[d].children[a].children[n].name);fr[sd.children[e].name][sd.children[e].children[d].name].push(r)}}}
+
+
+// async function BUILD_HD_REF( wMountPoint ) {
+// 	sd = directoryTree_Stage1( wMountPoint );
+// 	console.log( sd );
+// 	directoryTree_Stage2();
+// 	return fr;
+// }
+// module.exports.buildHardDriveReference = BUILD_HD_REF;
+
+function CustomDirTreeFilter( path ) {
+	const tree = dirTree( TestPath );
+
+	var Tree = {
+		"audiobooks": {} ,
+		"dvds": {} ,
+		"tvshows": {} ,
+		"movies": {} ,
+		"music": {} ,
+		"odyssey": {} ,
+		"documentaries": {} ,
+		"podcasts": {} ,
+	}
+	var finalGenres = {};
+
+	for ( var i = 0; i < tree.children.length; ++i ) { 
+
+		let genre = tree.children[ i ].name.toLowerCase();
+		console.log( "Sorting --> " + genre );
+
+		if ( Tree[ genre ] ) {  // Each Genre
+
+			Tree[ genre ] = tree.children[ i ].children;
+
+			var shows = {};
+			for ( var j = 0; j < Tree[ genre ].length; ++j ) {  // Each Show
+
+				let show_name = Tree[ genre ][ j ].name;
+				console.log( "\tSorting --> " + show_name );
+
+				shows[ show_name ] = [];
+
+				var seasons = {};
+				if ( Tree[ genre ][ j ].type === "file" ) {
+					//if ( !seasons[ "singles" ] ) { seasons[ "singles" ] = []; }
+					//seasons[ "singles" ].push( Tree[ genre ][ j ] );
+					shows[ show_name ].push( Tree[ genre ][ j ] );
+					continue;
+				}
+				
+				if ( Tree[ genre ][ j ].type !== "directory" ) { continue; }
+
+				for ( var k = 0; k < Tree[ genre ][ j ].children.length; ++k ) { // Each Season
+
+					let season_name = Tree[ genre ][ j ].children[ k ].name;
+					console.log( "\t\tSorting --> " + season_name );
+					let season = [];
+
+					if ( Tree[ genre ][ j ].children[ k ].type === "file" ) {
+						//console.log( "\t\t\t " + Tree[ genre ][ j ].children[ k ].name );
+						//shows[ show_name ][ season_name ].push( Tree[ genre ][ j ].children[ k ] );
+						season.push( Tree[ genre ][ j ].children[ k ] );
+						continue;
+					}
+
+					else if ( Tree[ genre ][ j ].children[ k ].type !== "directory" ) { continue; }
+
+					if ( Tree[ genre ][ j ].children[ k ].children ) {
+
+						for ( var e = 0; e < Tree[ genre ][ j ].children[ k ].children.length; ++e ) {
+							//console.log( "\t\t\t " + Tree[ genre ][ j ].children[ k ].children[ e ].name );
+							//console.log( "\t\t\t " + Tree[ genre ][ j ].children[ k ].children[ e ].path );
+							
+							//shows[ show_name ][ season_name ].push( Tree[ genre ][ j ].children[ k ].children[ e ] );
+							season.push( Tree[ genre ][ j ].children[ k ].children[ e ] );
+						}
+
+					}
+
+					shows[ show_name ].push( season );
+
+				}
+				// console.log( seasons );
+				finalGenres[ genre ] = shows;
+			}
+
+		}
+
+		//Tree[ genre ] = finalGenres;
+
+	}
+
+	return finalGenres;
+
+}
 
 
 function REBUILD_REDIS_MOUNT_POINT_REFERENCE( wMountPoint ) {
@@ -100,7 +186,8 @@ function REBUILD_REDIS_MOUNT_POINT_REFERENCE( wMountPoint ) {
 		try {
 			
 			// Scan Mount_Point
-			const x1 = await BUILD_HD_REF( wMountPoint );
+			//const x1 = await BUILD_HD_REF( wMountPoint );
+			const x1 = CustomDirTreeFilter( wMountPoint );
 			console.log( x1 );
 
 			// Each Genre
