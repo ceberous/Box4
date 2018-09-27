af3dc00fda66e2fd351bff0f1d087d929f7036e8
morpheous <cerbus.collin@gmail.com>
2018-09-27 12:46:45 -0700

    135

--- server/modules/buttons/Manager.js
+++ server/modules/buttons/Manager.js
@@ -14,3 +14,8 @@ const MPLAYER_MAN = require( path.join( MainFP , "server" , "modules" , "mplayer
 
 
 var G_NOW_PLAYING = G_R_Live_Genre_NP = G_R_NP_ShowName_Backup = null;
+
+// https://www.amazon.com/Black-Acrylic-Panel-Replacement-Arcade/dp/B075894V5M/
+// https://www.amazon.com/gp/product/B01MRWL6DW
+// https://www.amazon.com/gp/product/B00XHRIKLE
+// https://www.instructables.com/id/Make-Custom-Labels-for-Happ-Pushbuttons/
