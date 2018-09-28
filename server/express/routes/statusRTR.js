07dad8281e496655e26191374a80b4eff9f9d09a
morpheous <cerbus.collin@gmail.com>
2018-09-27 20:41:58 -0700

    161

--- server/express/routes/statusRTR.js
+++ server/express/routes/statusRTR.js
@@ -4,9 +4,5 @@ const router = express.Router();
 const statusCTRL = require( "../controllers/statusCTRL.js" );
 
 router.get( "/" , statusCTRL.all );
-// router.get( "/restart-pm2/" , specialCTRL.restartPM2 );
-// router.get( "/tv-power/" , specialCTRL.tvPower );
-
-// router.put( "/os/:task" , specialCTRL.osCommand  );
 
 module.exports = router;
