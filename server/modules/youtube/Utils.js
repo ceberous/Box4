2d671df053e93fc1306f521046c8be5fd714ed82
morpheous <cerbus.collin@gmail.com>
2018-09-28 16:59:41 -0700

    229

--- server/modules/youtube/Utils.js
+++ server/modules/youtube/Utils.js
@@ -10,18 +10,29 @@ function FILTER_GLOBAL_BLACKLIST_AND_WATCHED_AND_SKIPPED( wNewIDS ) {
 			const temp_blacklist_key = "TMP_KEY.BLACKLIST." + Math.random().toString(36).substring(7);
 			const temp_watched_key = "TMP_KEY.WATCHED." + Math.random().toString(36).substring(7);
 
+			let final_ids;
+			let skipped = await Redis.setAddArrayWithFilter( temp_skipped_key , RC.SKIPPED , wNewIDS );
+			if ( skipped ) { if skipped.length > 0 {
+				final_ids = wNewIDS.filter( x => skipped.indexOf( x ) === -1 );
+			}}
+			let blacklisted = await Redis.setAddArrayWithFilter( temp_blacklist_key , RC.GLOBAL_BLACK_LIST , final_ids );
+			if ( blacklisted ) { if blacklisted.length > 0 {
+				final_ids = final_ids.filter( x => blacklisted.indexOf( x ) === -1 );
+			}}			
+			await Redis.setAddArrayWithFilter( temp_watched_key , RC.WATCHED , final_ids );
+
 			// ( rInstance , wDestinationKey , wFilterSetKey , wArray )
-			var final_ids = await Redis.setAddArrayWithFilter( temp_skipped_key , RC.SKIPPED , wNewIDS );
-			if ( final_ids ) {
-				if ( final_ids.length > 0 ) {
-					final_ids = await Redis.setAddArrayWithFilter( temp_blacklist_key , RC.GLOBAL_BLACK_LIST , final_ids );
-					if ( final_ids ) {
-						if ( final_ids.length > 0 ) {
-							final_ids = await Redis.setAddArrayWithFilter( temp_watched_key , RC.WATCHED , final_ids );
-						}
-					}
-				}
-			}
+			// var final_ids = await Redis.setAddArrayWithFilter( temp_skipped_key , RC.SKIPPED , wNewIDS );
+			// if ( final_ids ) {
+			// 	if ( final_ids.length > 0 ) {
+			// 		final_ids = await Redis.setAddArrayWithFilter( temp_blacklist_key , RC.GLOBAL_BLACK_LIST , final_ids );
+			// 		if ( final_ids ) {
+			// 			if ( final_ids.length > 0 ) {
+			// 				final_ids = await Redis.setAddArrayWithFilter( temp_watched_key , RC.WATCHED , final_ids );
+			// 			}
+			// 		}
+			// 	}
+			// }
 			await Redis.keyDel( temp_skipped_key );
 			await Redis.keyDel( temp_watched_key );
 			await Redis.keyDel( temp_blacklist_key );
