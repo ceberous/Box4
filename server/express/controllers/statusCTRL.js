997d857b8f8cacfa6d3d1a823d59e369cc0c0eb4
morpheous <cerbus.collin@gmail.com>
2018-09-27 18:07:53 -0700

    145

--- server/express/controllers/statusCTRL.js
+++ server/express/controllers/statusCTRL.js
@@ -1,3 +1,7 @@
+const MainFP = process.mainModule.paths[ 0 ].split( "node_modules" )[ 0 ].slice( 0 , -1 );
+const path = require( "path" );
+const GetStatus = require( path.join( MainFP , "server" , "utils" , "Generic.js" ) ).getStatusReport;
+
 function sendJSONResponse( res , status , content ) { if ( status ) { res.status( status ); } res.json( content ); }
 
 module.exports.all = function( req , res ) { 
