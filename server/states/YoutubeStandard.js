ee7d079612930f9e9ff97ad4201aa2d4679ea456
morpheous <cerbus.collin@gmail.com>
2018-09-29 13:59:36 -0700

    324

--- server/states/YoutubeStandard.js
+++ server/states/YoutubeStandard.js
@@ -1,5 +1,4 @@
 const path = require( "path" );
-const FirefoxWrapper = require( "firefox-wrapper" );
 const MainFP = process.mainModule.paths[ 0 ].split( "node_modules" )[ 0 ].slice( 0 , -1 );
 const Reporter = require( path.join( MainFP , "server" , "utils" , "Reporter.js" ) );
 const Redis = require( path.join( MainFP , "main.js" ) ).redis;
