c1d2d7190449c749883ef997bd52a16247190b4c
morpheous <cerbus.collin@gmail.com>
2018-09-29 14:12:10 -0700

    337

--- server/states/YoutubeStandard.js
+++ server/states/YoutubeStandard.js
@@ -5,8 +5,7 @@ const Redis = require( path.join( MainFP , "main.js" ) ).redis;
 const RC = Redis.c.YOUTUBE;
 const wEmitter = require( path.join( MainFP , "main.js" ) ).emitter;
 const SetStagedFFClientTask = require( path.join( MainFP , "server" , "utils" , "Generic.js" ) ).setStagedFFClientTask;
-const FirefoxWrapper = require( "firefox-wrapper" );
-let FFManager = require( path.join( MainFP , "main.js" ) ).FFManager;
+const FFManager = require( path.join( MainFP , "server" , "modules" , "firefox" , "Firefox.js" ) );
 function GET_NEXT_VIDEO() {
 	return new Promise( async function( resolve , reject ) {
 		try {
