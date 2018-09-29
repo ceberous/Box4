5d808a99fef611ea71006b9d58aa962182a12806
morpheous <cerbus.collin@gmail.com>
2018-09-28 17:24:57 -0700

    251

--- server/modules/youtube/Utils.js
+++ server/modules/youtube/Utils.js
@@ -7,10 +7,9 @@ const Reporter = require( path.join( MainFP , "server" , "utils" , "Reporter.js"
 function FILTER_GLOBAL_BLACKLIST_AND_WATCHED_AND_SKIPPED( wNewIDS ) {
 	return new Promise( async function( resolve , reject ) {
 		try {
-			Reporter.log( RC );
-			const temp_skipped_key = "TMP_KEY.SKIPPED." + Math.random().toString(36).substring(7);
-			const temp_blacklist_key = "TMP_KEY.BLACKLIST." + Math.random().toString(36).substring(7);
-			const temp_watched_key = "TMP_KEY.WATCHED." + Math.random().toString(36).substring(7);
+			let temp_skipped_key = "TMP_KEY.SKIPPED." + Math.random().toString(36).substring(7);
+			let temp_blacklist_key = "TMP_KEY.BLACKLIST." + Math.random().toString(36).substring(7);
+			let temp_watched_key = "TMP_KEY.WATCHED." + Math.random().toString(36).substring(7);
 
 			let final_ids;
 			let skipped = await Redis.setAddArrayWithFilter( temp_skipped_key , RC.SKIPPED , wNewIDS );
@@ -25,19 +24,6 @@ function FILTER_GLOBAL_BLACKLIST_AND_WATCHED_AND_SKIPPED( wNewIDS ) {
 			if ( watched ) { if ( watched.length > 0 ) {
 				final_ids = final_ids.filter( x => watched.indexOf( x ) === -1 );
 			}}
-
-			// ( rInstance , wDestinationKey , wFilterSetKey , wArray )
-			// var final_ids = await Redis.setAddArrayWithFilter( temp_skipped_key , RC.SKIPPED , wNewIDS );
-			// if ( final_ids ) {
-			// 	if ( final_ids.length > 0 ) {
-			// 		final_ids = await Redis.setAddArrayWithFilter( temp_blacklist_key , RC.GLOBAL_BLACK_LIST , final_ids );
-			// 		if ( final_ids ) {
-			// 			if ( final_ids.length > 0 ) {
-			// 				final_ids = await Redis.setAddArrayWithFilter( temp_watched_key , RC.WATCHED , final_ids );
-			// 			}
-			// 		}
-			// 	}
-			// }
 			await Redis.keyDel( temp_skipped_key );
 			await Redis.keyDel( temp_watched_key );
 			await Redis.keyDel( temp_blacklist_key );
