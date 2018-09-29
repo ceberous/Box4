21b686149a209600c97a2ce4c63be83a0a074387
morpheous <cerbus.collin@gmail.com>
2018-09-29 14:15:10 -0700

    344

--- server/modules/firefox/Firefox.js
+++ server/modules/firefox/Firefox.js
@@ -21,3 +21,16 @@ function YOUTUBE() {
 	});
 }
 module.exports.youtube = YOUTUBE;
+
+function YOUTUBE_FULLSCREEN() {
+	return new Promise( async function( resolve , reject ) {
+		try {
+			FFManager.x.fullScreen();
+			FFManager.x.centerMouse();
+			await FFManager.sleep( 1000 );
+			FFManager.x.pressKeyboardKey( "f" );
+			resolve();
+		}
+		catch( error ) { console.log( error ); reject( error ); }
+	});	
+}
