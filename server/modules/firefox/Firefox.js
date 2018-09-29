80ea39a0737f4887daa71af6686624fd8ad972d8
morpheous <cerbus.collin@gmail.com>
2018-09-29 14:16:42 -0700

    346

--- server/modules/firefox/Firefox.js
+++ server/modules/firefox/Firefox.js
@@ -8,13 +8,7 @@ function YOUTUBE() {
 		try {
 			FFManager = new FirefoxWrapper();
 			await FFManager.launch();
-			await FFManager.openNewTab( "http://localhost:6969/youtube" );
-			FFManager.x.fullScreen();
-			FFManager.x.centerMouse();
-			await FFManager.sleep( 6000 );
-			FFManager.x.leftClick()
-			await FFManager.sleep( 500 );
-			FFManager.x.pressKeyboardKey( "f" );			
+			await FFManager.openNewTab( "http://localhost:6969/youtube" );		
 			resolve();
 		}
 		catch( error ) { console.log( error ); reject( error ); }
