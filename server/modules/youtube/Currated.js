742e31bc2f07428e98b9018ced02b93cca75b1c4
morpheous <cerbus.collin@gmail.com>
2018-09-28 17:39:53 -0700

    263

--- server/modules/youtube/Currated.js
+++ server/modules/youtube/Currated.js
@@ -1,5 +1,6 @@
-const MainFP = process.mainModule.paths[ 0 ].split( "node_modules" )[ 0 ].slice( 0 , -1 );
 const path = require( "path" );
+const MainFP = process.mainModule.paths[ 0 ].split( "node_modules" )[ 0 ].slice( 0 , -1 );
+const Reporter = require( path.join( MainFP , "server" , "utils" , "Reporter.js" ) );
 const Redis = require( path.join( MainFP , "main.js" ) ).redis;
 const RC = Redis.c.YOUTUBE.CURRATED;
 
