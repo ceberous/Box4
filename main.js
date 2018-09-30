0fc47022e5ebd5bc7e1fd763a096158d7f5dc2c7
morpheous <cerbus.collin@gmail.com>
2018-09-29 17:14:25 -0700

    351

--- main.js
+++ main.js
@@ -27,6 +27,23 @@ const ip = require( "ip" );
 const localIP = ip.address();
 const WebSocket = require( "ws" );
 const RMU = require( "redis-manager-utils" );
+const pidusage = require( "pidusage" )
+
+// Compute statistics every second:
+setInterval(function () {
+	pidusage( process.pid , function ( err , stats ) {
+	console.log( stats.memory + " === " + stats.cpu );
+	// => {
+	//   cpu: 10.0,            // percentage (from 0 to 100*vcore)
+	//   memory: 357306368,    // bytes
+	//   ppid: 312,            // PPID
+	//   pid: 727,             // PID
+	//   ctime: 867000,        // ms user + system time
+	//   elapsed: 6650000,     // ms since the start of the process
+	//   timestamp: 864000000  // ms since epoch
+	// }
+	});
+}, 1000 );
 
 ( async ()=> {
 
